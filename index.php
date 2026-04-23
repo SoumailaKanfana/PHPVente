3eda1d9fcac3bd68ca0998ea0ea15862bc90081d
SoumailaKanfana <kanfanasoumaila@gmail.com>
2026-04-23 16:56:50 +0000

    commit1

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include("config/db.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gestion Produits</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-dark text-white">
+
+<div class="container mt-5">
+    <h2 class="text-center">🛒 Gestion des Produits</h2>
+
+    <a href="pages/produits.php" class="btn btn-primary">Voir Produits</a>
+</div>
+
+
+    <?php
+    $cat = mysqli_query($conn, "SELECT * FROM categorie");
+    while ($c = mysqli_fetch_assoc($cat)) {
+        echo "<option value='".$c['id']."'>".$c['nom']."</option>";
+    }
+    ?>
+</select>
+
+</body>
+</html>
